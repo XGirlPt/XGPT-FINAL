d2c679eaf7b2057646ff33f15ed26c0826a36d72
XGPT <xgirlportugal@proton.me>
2025-02-10 13:48:33 +0100

    minor fixing2

--- src/app/_ui/hero-section.tsx
+++ src/app/_ui/hero-section.tsx
@@ -19,6 +19,7 @@ import { MdFiberManualRecord } from 'react-icons/md'; // Ícone de "Live"
 import { useTranslation } from 'react-i18next';
 import { useLanguage } from '../../context/LanguageContext'; 
 import { FaVideo, FaCrown, FaClock, FaCommentDots, FaMapMarkerAlt } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
 
 
 const fadeInUp = {
@@ -55,6 +56,7 @@ interface Profile {
   tagtimestamp: string;
   certificado: boolean;
   live: boolean | string;
+  premium: boolean | string;
   // live pode ser booleano ou string
 }
 
@@ -81,7 +83,7 @@ export function HeroSection({ profiles }: { profiles: Profile[] }) {
   const { language, changeLanguage } = useLanguage();
   
   return (
-    <section className="relative py-8 px-4">
+    <section className="relative py-8 md:px-4">
       {/* Small profile images on sides - can be implemented as floating decorations */}
 
       {/* Main content */}
@@ -160,8 +162,9 @@ export function HeroSection({ profiles }: { profiles: Profile[] }) {
               }}
             >
               <HeroImageContainer
-                src={profiles[2]?.photos[0] || '/logo.webp'}
+                src={profiles[5]?.photos[0] || '/logo.webp'}
                 alt="Profile picture"
+                
               />
             </motion.div>
           </Link>
@@ -204,81 +207,88 @@ export function HeroSection({ profiles }: { profiles: Profile[] }) {
               }),
             ]}
           >
-        <CarouselContent className="flex gap-4">
+
+
+            
+<CarouselContent className="flex gap-4 pb-4">
   {profiles.map((profile, index) => (
     <CarouselItem key={index} className="basis-1/2 md:basis-1/6">
-      <motion.div
-        variants={cardVariants}
-        whileHover={{
-          scale: 1.03,
-          transition: { duration: 0.2 },
-        }}
-        whileTap={{ scale: 0.98 }}
-        className="relative bg-white dark:bg-[#300d1b] rounded-2xl shadow-lg overflow-hidden cursor-pointer transition-all hover:shadow-2xl flex flex-col w-[200px] md:w-[220px] h-[340px]"
-      >
-        {/* Foto de perfil com Nome e Localidade sobrepostos */}
-        <motion.div className="relative w-full h-[65%] rounded-xl overflow-hidden">
-          <Image
-            src={profile.photos[0] || "/logo.webp"}
-            alt={profile.nome}
-            fill
-            className="object-cover"
-          />
+      <Link href={`/escort/${profile.nome}`} passHref>
+        <motion.div
+          variants={cardVariants}
+          whileHover={{
+            scale: 1.03,
+            transition: { duration: 0.2 },
+          }}
+          whileTap={{ scale: 0.98 }}
+          className="relative bg-pink-100 dark:bg-[#300d1b] rounded-2xl shadow-lg overflow-hidden cursor-pointer transition-all hover:shadow-2xl flex flex-col w-[200px] md:w-[220px] h-[340px]"
+        >
+          {/* Foto de perfil com Nome e Localidade sobrepostos */}
+          <motion.div className="relative w-full h-[65%] rounded-xl overflow-hidden">
+            <Image
+              src={profile.photos[0] || "/logo.webp"}
+              alt={profile.nome}
+              fill
+              className="object-cover w-full h-full"
+            />
             {/* Premium Badge */}
-                        <div className="absolute top-2 right-2 bg-yellow-600 text-white text-xs font-semibold py-1 px-2 rounded-full z-10 flex items-center  shadow-md">
-                          <FaCrown className="text-white mr-1" />
-                          <span className=" text-xs">Premium</span>
-                        </div>
-                        {/* Badges na imagem */}
-                        {profile.live && (
-                          <div className="absolute top-2 left-2 bg-red-700 text-white text-xs font-semibold py-1 px-2 rounded-full z-10 animate-pulse flex items-center">
-                            <MdFiberManualRecord className="text-white mr-1" />
-                            <span className="text-xs">Live Cam</span>
-                          </div>
-                        )}
-                        {Array.isArray(profile.stories) && profile.stories.length > 0 && (
-                          <div className="absolute top-10 right-2 md:right-3 bg-pink-800 text-white text-xs font-semibold py-1 px-2 rounded-full z-50 flex items-center">
-                            <FaVideo className="text-white mr-1 " />
-                            <span className="text-xs">Stories</span>
-                          </div>
-                        )}
-          {/* Gradiente e Nome + Localidade */}
-          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-3">
-            <h3 className="text-base md:text-lg font-semibold text-white leading-tight">
-              {profile.nome}
-            </h3>
-            <div className="flex items-center gap-1 text-white text-sm">
-              <FaMapMarkerAlt className="text-pink-600" />
-              {profile.cidade}
+            {profile.premium && (
+              <div className="absolute top-2 right-2 bg-yellow-600 text-white text-xs font-semibold py-1 px-2 rounded-full z-10 flex items-center shadow-md">
+                <FaCrown className="text-white mr-1" />
+                <span className="text-xs">Premium</span>
+              </div>
+            )}
+            {/* Badges na imagem */}
+            {profile.live && (
+              <div className="absolute top-2 left-2 bg-red-700 text-white text-xs font-semibold py-1 px-2 rounded-full z-10 animate-pulse flex items-center">
+                <MdFiberManualRecord className="text-white mr-1" />
+                <span className="text-xs">Live Cam</span>
+              </div>
+            )}
+            {Array.isArray(profile.stories) && profile.stories.length > 0 && (
+              <div className="absolute top-10 right-2 md:right-3 bg-pink-800 text-white text-xs font-semibold py-1 px-2 rounded-full z-50 flex items-center">
+                <FaVideo className="text-white mr-1" />
+                <span className="text-xs">Stories</span>
+              </div>
+            )}
+            {/* Gradiente e Nome + Localidade */}
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-3">
+            <h3 className="text-base md:text-lg font-semibold text-white leading-tight flex items-center gap-1">
+                {profile.nome} {profile.certificado && <FaCheckCircle className="text-green-500" />}
+              </h3>
+              <div className="flex items-center gap-1 text-white text-sm">
+                <FaMapMarkerAlt className="text-pink-600" />
+                {profile.cidade}
+              </div>
+            </div>
+          </motion.div>
+          
+          {/* Tag e Tempo */}
+          <div className="bg-pink-100 dark:bg-[#3a1a2a] text-gray-800 dark:text-gray-200 px-3 py-3 rounded-xl shadow-md mt-2 flex flex-col justify-between flex-1 min-h-[70px] relative">
+            <div className="flex items-start justify-between gap-2">
+              <span className="block break-words italic text-sm md:text-sm leading-tight max-h-[60px] overflow-hidden font-arial animate-flash">
+                &quot;{profile.tag}&quot;
+              </span>
+              <FaCommentDots className="text-yellow-600 text-md min-w-[18px] min-h-[18px] flex-shrink-0" />
+            </div>
+            <div className="text-xs font-arial text-black dark:text-gray-100 flex items-center gap-1 mt-2">
+              <FaClock className="text-yellow-500 h-4 w-4 font-normal" />
+              {timeAgo(profile.tagtimestamp)}
             </div>
           </div>
         </motion.div>
-
-        {/* Tag e Tempo */}
-        <div className="bg-pink-100 dark:bg-[#3a1a2a] text-gray-800 dark:text-gray-200 px-3 py-3 rounded-xl shadow-md italic text-xs md:text-sm mt-2 flex flex-col justify-between flex-1 min-h-[70px]">
-        <div className=" flex items-start justify-between gap-2">
-  <span className="block break-words leading-tight max-h-[60px] overflow-hidden">
-  &quot;{profile.tag}&quot;   
-  <FaCommentDots className="text-yellow-600 text-md " />
-
-  </span>
-</div>
-          <div className="text-base font-arial text-gray-200 flex items-center gap-1 mt-2">
-            <FaClock className="text-yellow-500 text-md font-normal" />
-            {timeAgo(profile.tagtimestamp)}
-          </div>
-        </div>
-      </motion.div>
+      </Link>
     </CarouselItem>
   ))}
 </CarouselContent>
 
 
 
+
             {/* Custom navigation buttons */}
-            <div className="flex justify-center gap-2 mt-8">
-              <CarouselPrevious className="static flex translate-x-0 translate-y-0 position-static w-8 h-8 rounded-full" />
-              <CarouselNext className="static flex translate-x-0 translate-y-0 position-static w-8 h-8 rounded-full" />
+            <div className="flex justify-center gap-2 mt-2">
+              <CarouselPrevious className="static flex translate-x-0 bg-white text-pink-600 dark:text-white dark:bg-black dark-text-white translate-y-0 position-static w-10 h-10 rounded-full" />
+              <CarouselNext className="static flex translate-x-0 bg-pink-600 hover:bg-pink-700 text-white translate-y-0 position-static w-10 h-10 rounded-full" />
             </div>
           </Carousel>
           </div>
